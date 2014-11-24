675def8427598defeefce9f5ef55255b24991094
JaredMichaelSmith <smith@utk.edu>
2014-11-24 10:29:02 -0500

    Added database_init_sample for database initialization

---
+++ src/database_init_sample.sh
@@ -0,0 +1,3 @@
+mysqldump --user=INSERT USER HERE --password=INSERT PASSWORD HERE twitter
+mysql --user=INSERT USER HERE --password=INSERT PASSWORD HERE twitter < database_init.sql
+mysql --user=INSERT USER HERE --password=INSERT PASSWORD HERE twitter < database_creator.sql
